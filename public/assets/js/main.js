b76cd1e2c88a31120a343a59b577829c2be707c1
Paul Fox <paul@homemadedigital.com>
2014-07-12 16:09:51 +0100

    Landing page complete

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -177,17 +177,43 @@ module.exports = Backbone.View.extend({
 
         this.$el.html(Templates["index"]({}));
 
-        $('.hexagon').each(function(){
-        	var el = $(this);
-
-        	window.setTimeout( function(){
-        							el.addClass("animated fadeIn")
-        			 		   },
-        					   Math.floor( ((Math.random() * 10)+1) * 100));
-        	
-        });
+        this.renderHexagons();
+        this.renderCharts();        
 
         return this;
+    },
+
+    renderHexagons: function() {
+
+        // Fade hexagons in
+        $('.hexagons').appear().on('appear', function(event, $all_appeared_elements) {
+
+            $('.hexagon').each(function(){
+                var el = $(this);
+
+                window.setTimeout( function(){
+                                        el.addClass("animated fadeIn")
+                                   },
+                                   Math.floor( ((Math.random() * 10)+1) * 100));
+                
+            });
+        });
+    },
+
+    renderCharts: function() {
+
+        Pizza.init();
+
+        Pizza.init("#chart-1,#chart-2,#chart-3",{
+            donut: true,
+            donut_inner_ratio: 0.6, 
+            percent_offset: 0,    
+            stroke_color: 'transparent',
+            stroke_width: 0,
+            animation_speed: 50,
+            animation_type: 'elastic',
+            always_show_text: true
+        });
     }
 });
 },{}]},{},[3])
