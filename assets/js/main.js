bdecc33a873a5807cdd3910b4cffbcae2cecc2f3
Paul Fox <paul@homemadedigital.com>
2014-07-13 13:42:18 +0100

    Improve charts

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,7 +229,25 @@ module.exports = Backbone.View.extend({
 
         this.$el.html(Templates["tech"]({}));
 
+        this.renderCharts();
+
         return this;
+    },
+
+     renderCharts: function() {
+
+        Pizza.init();
+
+        Pizza.init(".donut-chart",{
+            donut: true,
+            donut_inner_ratio: 0.6, 
+            percent_offset: 0,    
+            stroke_color: 'transparent',
+            stroke_width: 0,
+            animation_speed: 50,
+            animation_type: 'elastic',
+            always_show_text: true
+        });
     }
 });
 },{}]},{},[3])
