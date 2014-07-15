b619a1757af881689e1e35366e95f369bf0fac9e
Paul Fox <paul@homemadedigital.com>
2014-07-15 21:45:13 +0100

    Radar charts

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,6 +224,8 @@ module.exports = Backbone.View.extend({
     }
 });
 },{}],5:[function(require,module,exports){
+var visits = 0;
+
 module.exports = Backbone.View.extend({
 
     events: {
@@ -241,7 +243,10 @@ module.exports = Backbone.View.extend({
 
      renderCharts: function() {
 
-        Pizza.init();
+        if (visits > 0)
+            Pizza.init();
+
+        visits++;
 
         Pizza.init(".donut-chart",{
             donut: true,
@@ -253,6 +258,97 @@ module.exports = Backbone.View.extend({
             animation_type: 'elastic',
             always_show_text: true
         });
+
+        ///////////////////////////////////
+        // Charts.js implementations
+        ///////////////////////////////////
+
+        this.renderRadarChart( $("#language-radar"), 
+                                    {
+                                        labels: [ "Node.js", "PHP", "C/C++", "Python", "Ruby", "SQL", "Go"],
+                                        datasets: [
+                                            {
+                                                label: "Expertise",
+                                                fillColor: "rgba(97,53,115,0.85)",
+                                                strokeColor: "rgba(97,53,115,1)",
+                                                pointColor: "white",
+                                                pointStrokeColor: "rgba(97,53,115,1)",
+                                                pointHighlightFill: "#fff",
+                                                pointHighlightStroke: "rgba(220,220,220,1)",
+                                                data: [75, 99, 75, 85, 78, 90, 100]
+                                            },
+                                            {
+                                                label: "Love",
+                                                fillColor: "rgba(42,152,143,0.75)",
+                                                strokeColor: "rgba(42,152,143,1)",
+                                                pointColor: "white",
+                                                pointStrokeColor: "rgba(42,152,143,1",
+                                                pointHighlightFill: "#fff",
+                                                pointHighlightStroke: "rgba(151,187,205,1)",
+                                                data: [68, 85, 75, 55, 100, 70, 100]
+                                            },
+                                        ]
+                                    });
+
+        this.renderRadarChart( $("#paradigm-radar"), 
+                                    {
+                                        labels: [ "MVC", "AJAX", "Rich", "Mobile", "Scaling", "REST", "TDD" ],
+                                        datasets: [
+                                            {
+                                                label: "Expertise",
+                                                fillColor: "rgba(97,53,115,0.85)",
+                                                strokeColor: "rgba(97,53,115,1)",
+                                                pointColor: "white",
+                                                pointStrokeColor: "rgba(97,53,115,1)",
+                                                pointHighlightFill: "#fff",
+                                                pointHighlightStroke: "rgba(220,220,220,1)",
+                                                data: [ 85, 95, 78, 99, 95, 90, 100]
+                                            },
+                                            {
+                                                label: "Love",
+                                                fillColor: "rgba(42,152,143,0.75)",
+                                                strokeColor: "rgba(42,152,143,1)",
+                                                pointColor: "white",
+                                                pointStrokeColor: "rgba(42,152,143,1",
+                                                pointHighlightFill: "#fff",
+                                                pointHighlightStroke: "rgba(151,187,205,1)",
+                                                data: [100, 68, 75, 90, 80, 100, 85, ]
+                                            },
+                                        ]
+                                    });
+    },
+
+    renderRadarChart: function(el, data) {
+
+        /// Get context with jQuery - using jQuery's .get() method.
+        var ctx = el.get(0).getContext("2d");
+
+        var width = $('canvas').parent().width();
+
+        var options = {
+            responsive: true,
+            angleLineColor : "rgba(255,255,255,1)",
+            pointLabelFontColor : "white",
+            pointLabelFontSize : 14,
+            pointLabelFontFamily : "'Helvetica'",
+            pointLabelFontStyle : "bold",
+            scaleLineColor: "white",
+            scaleLineWidth: 2,
+            scaleShowLabels : false
+        };
+
+        el.attr("width",width);
+        el.attr("height",width);
+
+        $( window ).resize(function(event){
+            var width = el.parent().width();
+            el.attr("width",width);
+            el.attr("height",width)
+            radar = new Chart(ctx).Radar(data,options);
+        });
+
+        // This will get the first returned node in the jQuery collection.
+        var radar = new Chart(ctx).Radar(data, options);
     }
 });
 },{}]},{},[3])
